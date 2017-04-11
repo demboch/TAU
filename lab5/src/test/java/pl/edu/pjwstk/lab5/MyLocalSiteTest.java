5f348ced0594cbc86f66caefcf22ce3723dfef99
demboch <dembochdc@gmail.com>
2017-04-11 10:14:31 +0200

    kupa

--- lab5/src/test/java/pl/edu/pjwstk/lab5/MyLocalSiteTest.java
+++ lab5/src/test/java/pl/edu/pjwstk/lab5/MyLocalSiteTest.java
@@ -1,5 +1,6 @@
 package pl.edu.pjwstk.lab5;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -13,7 +14,9 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -22,12 +25,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MyLocalSiteTest {
 
 	private static WebDriver driver;
 	WebElement element;
-	int i = 30;
+	int i = 38;
 
 	@BeforeClass
 	public static void driverSetup() {
@@ -38,7 +41,7 @@ public class MyLocalSiteTest {
 	}
 
 	@Test
-	public void homePage(){
+	public void test1homePage(){
 		driver.manage().window().maximize();
 		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
 
@@ -47,45 +50,18 @@ public class MyLocalSiteTest {
 	}
 
 	@Test
-	public void loginLogoutScreenshots(){
+	public void test2showLoginPage()throws InterruptedException {
 		driver.manage().window().maximize();
 		driver.get("http://localhost/ProjektTIN/index.php");
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/1przedLogowaniem.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
 
 		driver.findElement(By.linkText("Zaloguj")).click();
 		element = driver.findElement(By.name("password"));
-		element.sendKeys("haslo");
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/2wTrakcieLogowania.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
+		assertEquals("podaj hasło", element.getAttribute("placeholder"));
 
-		element.submit();
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		assertNotNull(screenshot);
 		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/3zalogowanie.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
-
-		driver.findElement(By.linkText("Wyloguj")).click();
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/4wylogowanie.png"));
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/1pokazStrone.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			assertTrue(false);
@@ -93,7 +69,153 @@ public class MyLocalSiteTest {
 	}
 
 	@Test
-	public void addNewPostPage() throws InterruptedException, AWTException {
+	public void test3checkIncorrectPassword()throws InterruptedException {
+		driver.manage().window().maximize();
+		driver.get("http://localhost/ProjektTIN/index.php");
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/2przedLogowaniem.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+
+		driver.findElement(By.linkText("Zaloguj")).click();
+		element = driver.findElement(By.name("password"));
+		element.sendKeys("zle_haslo");
+		element.submit();
+
+		WebElement element2 = driver.findElement(By.name("password"));
+		assertNotNull(element2);
+		Thread.sleep(2000);
+		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/3zleHaslo.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
+	@Test
+	public void test4checkCorrectPassword()throws InterruptedException {
+		driver.manage().window().maximize();
+		driver.get("http://localhost/ProjektTIN/index.php");
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/4przedLogowaniem.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+
+		driver.findElement(By.linkText("Zaloguj")).click();
+		element = driver.findElement(By.name("password"));
+		element.sendKeys("haslo");
+		Thread.sleep(2000);
+		screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/5wTrakcieLogowania.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+		element.submit();
+
+		element = driver.findElement(By.linkText("Wyloguj"));
+		assertNotNull(element);
+
+		driver.findElement(By.linkText("GunShop")).click();
+		Thread.sleep(2000);
+		screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/6glowna.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+
+		driver.findElement(By.linkText("Dodaj post")).click();
+		Thread.sleep(2000);
+		screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/7dodajPost.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+		driver.findElement(By.linkText("GunShop")).click();
+
+		driver.findElement(By.linkText("Wyloguj")).click();
+		Thread.sleep(2000);
+		screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		assertNotNull(screenshot);
+		try {
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/8wyloguj.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
+//	@Test
+//	public void test2loginLogoutScreenshots()throws InterruptedException {
+//		driver.manage().window().maximize();
+//		driver.get("http://localhost/ProjektTIN/index.php");
+//		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/1przedLogowaniem.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//
+//		driver.findElement(By.linkText("Zaloguj")).click();
+//		element = driver.findElement(By.name("password"));
+//		element.sendKeys("haslo");
+//
+//		Thread.sleep(2000);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/2wTrakcieLogowania.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//
+//		element.submit();
+//		element = driver.findElement(By.linkText("Wyloguj"));
+//		assertNotNull(element);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/3zalogowanie.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//
+//		driver.findElement(By.linkText("Wyloguj")).click();
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/4wylogowanie.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//	}
+//
+	@Test
+	public void test5addNewPostPage() throws InterruptedException, AWTException {
 		driver.manage().window().maximize();
 		driver.get("http://localhost/ProjektTIN/index.php");
 		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
@@ -103,13 +225,16 @@ public class MyLocalSiteTest {
 		element.sendKeys("haslo");
 		element.submit();
 
+		element = driver.findElement(By.linkText("Wyloguj"));
+		assertNotNull(element);
+
 		driver.get("http://localhost/ProjektTIN/index.php?akcja=dodajpostform");
 		element = driver.findElement(By.name("tresc"));
 		element.sendKeys("bekonowa amunicja");
 		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		assertNotNull(screenshot);
 		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/5trescPost.png"));
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/9trescPost.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			assertTrue(false);
@@ -139,118 +264,126 @@ public class MyLocalSiteTest {
 		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		assertNotNull(screenshot);
 		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/6dodanyPost.png"));
+			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/10dodanyPost.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			assertTrue(false);
 		}
 	}
+//
+//	@Test
+//	public void test4editNewPostPage() throws InterruptedException, AWTException {
+//		driver.manage().window().maximize();
+//		driver.get("http://localhost/ProjektTIN/index.php");
+//		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//
+//		driver.findElement(By.linkText("Zaloguj")).click();
+//		element = driver.findElement(By.name("password"));
+//		element.sendKeys("haslo");
+//		element.submit();
+//
+//		element = driver.findElement(By.linkText("Wyloguj"));
+//		assertNotNull(element);
+//
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
+//		Robot robot = new Robot();
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		Thread.sleep(2000);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/7przedEdycja.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//
+//		//driver.getCurrentUrl();
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=potwierdzenieedycji&przedmiotakcji=" + i);
+//		element = driver.findElement(By.name("tresc"));
+//		element.sendKeys("jednak beconBomb					");
+//		driver.findElement(By.className("uploadedytuj")).click();
+//		Thread.sleep(2000);
+//		StringSelection ss = new StringSelection("C:\\Users\\dembo\\Desktop\\PJWSTK\\SEMESTR6\\TAU\\bacon.jpg");
+//		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+//		robot.keyPress(KeyEvent.VK_ENTER);
+//		robot.keyRelease(KeyEvent.VK_ENTER);
+//		robot.keyPress(KeyEvent.VK_CONTROL);
+//		robot.keyPress(KeyEvent.VK_V);
+//		robot.keyRelease(KeyEvent.VK_V);
+//		robot.keyRelease(KeyEvent.VK_CONTROL);
+//		robot.keyPress(KeyEvent.VK_ENTER);
+//		robot.keyRelease(KeyEvent.VK_ENTER);
+//		driver.findElement(By.name("submit")).click();
+//
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		Thread.sleep(2000);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/8poEdycji.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//	}
+//
+//	@Test
+//	public void test5deleteNewPostPage() throws InterruptedException, AWTException{
+//		driver.manage().window().maximize();
+//		driver.get("http://localhost/ProjektTIN/index.php");
+//		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//
+//		driver.findElement(By.linkText("Zaloguj")).click();
+//		element = driver.findElement(By.name("password"));
+//		element.sendKeys("haslo");
+//		element.submit();
+//
+//		element = driver.findElement(By.linkText("Wyloguj"));
+//		assertNotNull(element);
+//
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
+//		Robot robot = new Robot();
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		Thread.sleep(2000);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/9jestPost.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//
+//		//driver.getCurrentUrl();
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=potwierdzenieusuniecia&przedmiotakcji=" + i);
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=usunpost&przedmiotakcji=" + i);
+//		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+//		Thread.sleep(2000);
+//		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		assertNotNull(screenshot);
+//		try {
+//			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/10usunietyPost.png"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//	}
 
 	@Test
-	public void editNewPostPage() throws InterruptedException, AWTException {
-		driver.manage().window().maximize();
-		driver.get("http://localhost/ProjektTIN/index.php");
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-
-		driver.findElement(By.linkText("Zaloguj")).click();
-		element = driver.findElement(By.name("password"));
-		element.sendKeys("haslo");
-		element.submit();
-
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
-		Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		Thread.sleep(2000);
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/7przedEdycja.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
-
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=potwierdzenieedycji&przedmiotakcji=" + i);
-		element = driver.findElement(By.name("tresc"));
-		element.sendKeys("jednak beconBomb					");
-		driver.findElement(By.className("uploadedytuj")).click();
-		Thread.sleep(2000);
-		StringSelection ss = new StringSelection("C:\\Users\\dembo\\Desktop\\PJWSTK\\SEMESTR6\\TAU\\bacon.jpg");
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.keyPress(KeyEvent.VK_CONTROL);
-		robot.keyPress(KeyEvent.VK_V);
-		robot.keyRelease(KeyEvent.VK_V);
-		robot.keyRelease(KeyEvent.VK_CONTROL);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		driver.findElement(By.name("submit")).click();
-
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		Thread.sleep(2000);
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/8poEdycji.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
-	}
-
-	@Test
-	public void deleteNewPostPage() throws InterruptedException, AWTException{
-		driver.manage().window().maximize();
-		driver.get("http://localhost/ProjektTIN/index.php");
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-
-		driver.findElement(By.linkText("Zaloguj")).click();
-		element = driver.findElement(By.name("password"));
-		element.sendKeys("haslo");
-		element.submit();
-
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
-		Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		Thread.sleep(2000);
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/9jestPost.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
-
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=potwierdzenieusuniecia&przedmiotakcji=" + i);
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=usunpost&przedmiotakcji=" + i);
-		driver.get("http://localhost/ProjektTIN/index.php?akcja=brak");
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
-		Thread.sleep(2000);
-		screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		assertNotNull(screenshot);
-		try {
-			FileUtils.copyFile(screenshot, new File("C:/Users/dembo/Desktop/PJWSTK/SEMESTR6/TAU/Lab5Screenshot/10usunietyPost.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
-	}
-
-	@Test
-	public void projektPHPtest() throws InterruptedException {
+	public void test6projektPHPtest() throws InterruptedException {
 		driver.manage().window().maximize();
 		driver.get("http://szuflandia.pjwstk.edu.pl/~s12902/ProjektPHP/");
 		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
